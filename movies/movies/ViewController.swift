469488cfa094a12192c5eead54a6de4b43986562
Shady Abdelaziz <shady.abdelaziz@blink22.com>
2020-06-13 19:47:33 +0200

    creating movie master-detail project

---
+++ movies/movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Shady Abdelaziz on 6/13/20.
+//  Copyright © 2020 SWVL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
